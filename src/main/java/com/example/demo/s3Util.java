7aade55639be24eca6650ad61360467223de40ae
Van Hung <hungnguyenvan209@gmail.com>
2022-07-17 18:37:22 +0700

    feature getAllBucket and create Bucket

--- src/main/java/com/example/demo/s3Util.java
+++ src/main/java/com/example/demo/s3Util.java
@@ -6,23 +6,25 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.Bucket;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
 
 @Service
 public class s3Util {
-    private final String BUCKET_NAME = "fdas";
+    private final String BUCKET_NAME = "sandbox";
     private final String accessKey = "sandbox";
     private final String secretKey = "s3cr3t";
-    BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey,secretKey);
+    BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
 
     final AmazonS3 s3 = AmazonS3ClientBuilder.standard()
             .withCredentials(new AWSCredentialsProvider() {
@@ -33,10 +35,9 @@ public class s3Util {
 
                 @Override
                 public void refresh() {
-
                 }
             })
-            .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://127.0.0.1:8000","default"))
+            .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://127.0.0.1:8000", "default"))
             .build();
 
     public static String generateFileName(MultipartFile multiPart) {
@@ -51,17 +52,35 @@ public class s3Util {
         return file;
     }
 
-    public String uploadFile(MultipartFile multipartFile){
+    public String uploadFile(MultipartFile multipartFile) {
         String fileUrl;
+        String fileName = generateFileName(multipartFile);
         try {
-            PutObjectRequest request = new PutObjectRequest(BUCKET_NAME,generateFileName(multipartFile),convertMultipartToFile(multipartFile));
+            PutObjectRequest request = new PutObjectRequest(BUCKET_NAME, fileName, convertMultipartToFile(multipartFile));
 //            s3.putObject(BUCKET_NAME,generateFileName(multipartFile),convertMultipartToFile(multipartFile));
-            s3.putObject(request.withCannedAcl(CannedAccessControlList.PublicReadWrite));
-            fileUrl = "http://127.0.0.1:8000/fdas/"+generateFileName(multipartFile);
+            s3.putObject(request.withCannedAcl(CannedAccessControlList.PublicRead));
+            fileUrl = "http://127.0.0.1:8000/" + BUCKET_NAME + "/" + fileName;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
         return fileUrl;
     }
 
+    public String createBucket(String bucketName) {
+        String fileUrl = null;
+        Bucket bucket;
+        if (s3.doesBucketExistV2(bucketName)) {
+            fileUrl = "error";
+            String status = HttpStatus.RESET_CONTENT.toString();
+            return status;
+        } else {
+            bucket = s3.createBucket(bucketName);
+            fileUrl = "http://localhost:5000/objects.html?bucket=" + bucket.getName();
+        }
+        return fileUrl;
+    }
+
+    public ArrayList<Bucket> getListBucket() {
+        return (ArrayList<Bucket>) s3.listBuckets();
+    }
 }
--- src/main/java/com/example/demo/uploadFileController.java
+++ src/main/java/com/example/demo/uploadFileController.java
@@ -1,13 +1,13 @@
 package com.example.demo;
 
+import com.amazonaws.services.s3.model.Bucket;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/upload")
@@ -16,9 +16,21 @@ public class uploadFileController {
 
     private final s3Util util;
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public String uploadFile(@RequestParam("file")MultipartFile multipartFile){
         String filename = s3Util.generateFileName(multipartFile);
 
         return util.uploadFile(multipartFile);
     }
+    @PostMapping("/{bucketName}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String createBucket(@PathVariable String bucketName){
+        return util.createBucket(bucketName);
+    }
+
+    @GetMapping()
+    public ArrayList<Bucket> getAllBucket(){
+        return util.getListBucket();
+    }
+
 }
